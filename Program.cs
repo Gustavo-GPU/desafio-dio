4c2e8127f9991404d335d1f0bdd21e4ac48ceea7
Gustavo Ultramari <Gust_GPU@hotmail.com>
2023-12-18 17:26:13 -0300

    Classes iPhone e Nokia, finalizadas, codigo do program foi gerado, correções na classe Smartphone.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smarthphone Nokia:");
+Smartphone nokia = new Nokia (numero: "1234-5678", modelo: "Modelo X", imei: "123456", memoria: 64 );
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone (numero: "5678-1234", modelo: "Modelo Y", imei: "123123", memoria: 128 );
+iphone.Ligar();
+iphone.InstalarAplicativo("Canva");
